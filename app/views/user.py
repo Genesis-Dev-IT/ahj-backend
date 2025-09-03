ebc92b4b6cfe7c72e5af46758ba731b54874a27c
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2025-09-03 20:17:48 +0530

    Supabase Integrations 1. add function to create and delete user in supabase

--- app/views/user.py
+++ app/views/user.py
@@ -10,41 +10,65 @@ from django.shortcuts import get_object_or_404
 from django.http import Http404
 from genesis.utils import chain_errors
 from django.db import transaction
-from genesis.utils import current_timestamp
+from genesis.supabase_client import get_supabase_client
+
+import logging
+logger = logging.getLogger(__name__)
+
+
 @method_decorator(csrf_exempt, name="dispatch")
 class UserDetailView(View):
     def post(self, request):
+        response = None
         try:
-            # user can be created by admin only
-
             body = JSONParser().parse(request)
-            serializer = UserSerializer(data=body)
-            if serializer.is_valid():
-                serializer.save()
+            supabase_client = get_supabase_client()
+
+            with transaction.atomic():
+                serializer = UserSerializer(data=body)
+                if serializer.is_valid():
+                    response = supabase_client.auth.admin.create_user(
+                        {
+                            "email": body.get("email"),
+                            "password": "Password@1",
+                        }
+                    )
+
+                    if not response.user:
+                        logger.error(f"Failed to create user in Supabase: {str(response)}")
+                        return JsonResponse({
+                            "error": "SUPABASE_ERROR",
+                            "message":"Failed to create user in Supabase"
+                        }, status=400)
+                
+                    serializer.save()
+                    return JsonResponse(
+                        {
+                            "error":None,
+                            "message":"User created successfully",
+                            "data":serializer.data
+                        }, status=status.HTTP_201_CREATED
+                    )
+                
+                supabase_client.auth.admin.delete_user(response.user.id)
+                error_messages = chain_errors(serializer.errors.values())
                 return JsonResponse(
                     {
-                        "error":None,
-                        "message":"User created successfully",
-                        "data":serializer.data
-                    }, status=status.HTTP_201_CREATED
-                )
-
-            # error_messages = list(chain.from_iterable(serializer.errors.values()))
-            error_messages = chain_errors(serializer.errors.values())
-            return JsonResponse(
-                {
-                    "error": "VALIDATION_ERROR",
-                    "message": " | ".join(error_messages)
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )   
+                        "error": "VALIDATION_ERROR",
+                        "message": " | ".join(error_messages)
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )  
         except Exception as e:
+            if response:
+                supabase_client.auth.admin.delete_user(response.user.id)
             print(e)
             return JsonResponse({
                 "error": "SERVER_ERROR",
                 "message": "Something went wrong while creating user. Please try again later."
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+
+        
     def get(self, request, id):
         try:
             user = get_object_or_404(User, id=id)
@@ -87,8 +111,7 @@ class UserDetailView(View):
                         if tokens.exists():
                             tokens.update(
                                 active=body["is_active"],
-                                updated_by=user,
-                                updated_at=current_timestamp()
+                                updated_by=user
                             )
                         else:
                             print(f"No active token found for user {user.id}")
@@ -119,10 +142,34 @@ class UserDetailView(View):
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     def delete(self, request, id):
+        page = 1
+        per_page = 1000
+        supabase_user = None
         try:
-            # user can be deleted by admin only
             user = get_object_or_404(User, id=id)
-            user.delete()
+            supabase_client = get_supabase_client()
+            while True:
+                response = supabase_client.auth.admin.list_users(page=page, per_page=per_page)
+                print(response)
+                if not response:
+                    break
+                
+                for sup_user in response:
+                    if sup_user.email == user.email:
+                        supabase_user = sup_user
+                        break;
+                if supabase_user:
+                    break;
+                if len(response) < per_page:
+                    break  # reached last page
+
+                page += 1
+
+            with transaction.atomic():
+                user.delete()
+
+            if supabase_user:
+                supabase_client.auth.admin.delete_user(supabase_user.id)
             return JsonResponse({
                 "error":None,
                 "message": f"User with id {id} deleted successfully."
@@ -138,9 +185,9 @@ class UserDetailView(View):
             print("Unexpected error:", e)
             return JsonResponse({
                 "error": "SERVER_ERROR",
-                "message": "Something went wrong while deleting user. Please try again later."
+                "message": "Something went wrong while deleting user."
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
+            
 class UserListView(View):
     def get(seld, request):
         pass
